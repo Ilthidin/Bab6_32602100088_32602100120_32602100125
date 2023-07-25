0cb0f3c027e45627a2c8fd8b187205b1778115db
Ilthidin <69728813+Ilthidin@users.noreply.github.com>
2023-07-25 15:16:33 +0700

    Upload Tampilan Utama

---
+++ template.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="/CSS/crud.css">
+     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.4.0/css/all.css">
+     <title>CRUD</title>
+</head>
+
+<body>
+     <header>
+          <nav>
+               <table>
+                    <tr>
+                         <td>Nama</td>
+                         <td> : </td>
+                         <td>Muhammad Syihab Habibi / C / Praktikum Web Programming</td>
+                    </tr>
+                    <tr>
+                         <td>NIM</td>
+                         <td> : </td>
+                         <td>32602100088</td>
+                    </tr>
+               </table>
+          </nav>
+     </header>
+     <?php $this->rendersection('content') ?>
+</body>
+
+</html>
